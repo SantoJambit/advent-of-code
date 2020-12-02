ce33c878105f2ab9667c289620fa9988f9b1cb4e
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-12-02 15:44:38 +0100

    day01 part 2

--- src/day01.spec.ts
+++ src/day01.spec.ts
@@ -1,22 +1,44 @@
-import { findAnswer, findSummands } from './day01';
+import { findAnswer, findAnswer2, findSummands, findSummands2 } from './day01';
 import { loadFile } from './lib';
 
+const exampleInput = [1721, 979, 366, 299, 675, 1456];
+
 describe('findSummands', () => {
-    test('should find summands', () => {
-        expect(findSummands(2020, [1721, 979, 366, 299, 675, 1456])).toEqual([1721, 299]);
+    test('should find the example summands', () => {
+        expect(findSummands(2020, exampleInput)).toEqual([1721, 299]);
     });
 });
 
 describe('findAnswer', () => {
-    test('should find the answer', () => {
-        expect(findAnswer(2020, [1721, 979, 366, 299, 675, 1456])).toBe(514579);
+    test('should find the example answer', () => {
+        expect(findAnswer(2020, exampleInput)).toBe(514579);
     });
-    test('should find the answer', () => {
+    test('should find the puzzzle answer', () => {
         expect(
             findAnswer(
                 2020,
                 loadFile('day01.input', (s) => parseInt(s, 10)),
             ),
         ).toBe(788739);
+    });
+});
+
+describe('findSummands2', () => {
+    test('should find the example summands', () => {
+        expect(findSummands2(2020, exampleInput)).toEqual([979, 366, 675]);
+    });
+});
+
+describe('findAnswer2', () => {
+    test('should find the example answer', () => {
+        expect(findAnswer2(2020, exampleInput)).toBe(241861950);
+    });
+    test('should find the puzzle answer', () => {
+        expect(
+            findAnswer2(
+                2020,
+                loadFile('day01.input', (s) => parseInt(s, 10)),
+            ),
+        ).toBe(178724430);
     });
 });
--- src/day01.ts
+++ src/day01.ts
@@ -10,3 +10,19 @@ export function findAnswer(sum: number, possibleSummands: number[]) {
     const [a, b] = findSummands(sum, possibleSummands);
     return a * b;
 }
+
+export function findSummands2(sum: number, possibleSummands: number[]) {
+    for (const a of possibleSummands) {
+        for (const b of possibleSummands) {
+            const ab = a + b;
+            const c = possibleSummands.find((s) => ab + s === sum);
+            if (c || c === 0) return [a, b, c];
+        }
+    }
+    return null;
+}
+
+export function findAnswer2(sum: number, possibleSummands: number[]) {
+    const [a, b, c] = findSummands2(sum, possibleSummands);
+    return a * b * c;
+}
