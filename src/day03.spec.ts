58fd10b0ca3e464e63340c25b2b43408bba1e898
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-12-03 11:09:40 +0100

    day03 part 2

--- src/day03.spec.ts
+++ src/day03.spec.ts
@@ -1,5 +1,5 @@
 import { countTreesForSlope } from './day03';
-import { loadFile } from './lib';
+import { loadFile, multiplyArray } from './lib';
 
 const exampleInput = [
     '..##.......',
@@ -15,11 +15,24 @@ const exampleInput = [
     '.#..#...#.#',
 ];
 
+const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+];
+
 describe('countTreesForSlope', () => {
     test('should count trees for example input correctly', () => {
-        expect(countTreesForSlope(exampleInput, 3, 1)).toBe(7);
+        const trees = slopes.map(([x, y]) => countTreesForSlope(exampleInput, x, y));
+        expect(trees).toEqual([2, 7, 3, 4, 2]);
+        expect(multiplyArray(trees)).toBe(336);
     });
     test('should count trees for puzzle input correctly', () => {
-        expect(countTreesForSlope(loadFile('day03.input'), 3, 1)).toBe(252);
+        const puzzleInput = loadFile('day03.input');
+        const trees = slopes.map(([x, y]) => countTreesForSlope(puzzleInput, x, y));
+        expect(trees).toEqual([57, 252, 64, 66, 43]);
+        expect(multiplyArray(trees)).toBe(2608962048);
     });
 });
