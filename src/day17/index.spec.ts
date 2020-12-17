7b1c77cf0f9508b49278b9503fa6ad4b6ef21e22
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-12-17 18:08:21 +0100

    n-dimensional. bit slower but more versatile

--- src/day17/index.spec.ts
+++ src/day17/index.spec.ts
@@ -1,4 +1,4 @@
-import { getCubeNeighbors3, getCubeNeighbors4, loadLayer, part1, part2, runCycle } from '.';
+import { loadLayer, part1, part2, runCycle } from '.';
 import { loadFile } from '../lib';
 
 const exampleInput3 = loadLayer(loadFile('day17/example.txt'), "0");
@@ -7,21 +7,21 @@ const exampleInput4 = loadLayer(loadFile('day17/example.txt'), "0,0");
 describe('runCycle', () => {
     describe('with 3 dimensions', () => {
         test('should return the active cubes after 1 cycle with the example input', () => {
-            expect(runCycle(exampleInput3, getCubeNeighbors3).size).toBe(11);
+            expect(runCycle(exampleInput3).size).toBe(11);
         });
         test('should return the active cubes after 6 cycle with the example input', () => {
             let next = exampleInput3;
-            for (let i = 0; i < 6; i++) next = runCycle(next, getCubeNeighbors3);
+            for (let i = 0; i < 6; i++) next = runCycle(next);
             expect(next.size).toBe(112);
         });
     });
     describe('with 4 dimensions', () => {
         test('should return the active hypercubes after 1 cycle with the example input', () => {
-            expect(runCycle(exampleInput4, getCubeNeighbors4).size).toBe(29);
+            expect(runCycle(exampleInput4).size).toBe(29);
         });
         test('should return the active hypercubes after 6 cycle with the example input', () => {
             let next = exampleInput4;
-            for (let i = 0; i < 6; i++) next = runCycle(next, getCubeNeighbors4);
+            for (let i = 0; i < 6; i++) next = runCycle(next);
             expect(next.size).toBe(848);
         });
     });
--- src/day17/index.ts
+++ src/day17/index.ts
@@ -11,20 +11,25 @@ export function loadLayer(lines: string[], suffixCoords: string) {
     return cubes;
 }
 
-export function getCubeNeighbors3(cube: string): string[] {
-    const [x, y, z] = cube.split(',').map((s) => parseInt(s));
-    const neighbors: string[] = [];
-    for (let dx = x - 1; dx < x + 2; dx++) {
-        for (let dy = y - 1; dy < y + 2; dy++) {
-            for (let dz = z - 1; dz < z + 2; dz++) {
-                neighbors.push(`${dx},${dy},${dz}`);
-            }
+function getCubeNeighbors(cube: string): string[] {
+    const coordinates = cube.split(',').map((s) => parseInt(s));
+    let prefixes: number[][] = [[]];
+    for (const c of coordinates) {
+        const a = c - 1;
+        const b = c + 1;
+        const newPrefixes = [];
+        for (const prefix of prefixes) {
+            newPrefixes.push([...prefix, a]);
+            newPrefixes.push([...prefix, b]);
+            newPrefixes.push([...prefix, c]);
         }
+        prefixes = newPrefixes;
     }
+    const neighbors = prefixes.map((values) => values.join(','));
     return neighbors.filter((n) => n !== cube);
 }
 
-export function runCycle(cubes: Set<string>, getCubeNeighbors: (cube: string) => string[]) {
+export function runCycle(cubes: Set<string>) {
     const cubesAfter = new Set<string>();
     const touchedNeighbors: { [s: string]: number } = {};
     for (const cube of cubes) {
@@ -39,7 +44,8 @@ export function runCycle(cubes: Set<string>, getCubeNeighbors: (cube: string) =>
             }
         }
         // If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube becomes inactive.
-        if (activeNeighbors === 2 || activeNeighbors === 3) cubesAfter.add(cube);
+        if (activeNeighbors === 2 || activeNeighbors === 3)
+            cubesAfter.add(cube);
     }
     // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains inactive.
     for (const cube in touchedNeighbors) {
@@ -48,32 +54,17 @@ export function runCycle(cubes: Set<string>, getCubeNeighbors: (cube: string) =>
     return cubesAfter;
 }
 
-export function getCubeNeighbors4(cube: string): string[] {
-    const [x, y, z, w] = cube.split(',').map((s) => parseInt(s));
-    const neighbors: string[] = [];
-    for (let dx = x - 1; dx < x + 2; dx++) {
-        for (let dy = y - 1; dy < y + 2; dy++) {
-            for (let dz = z - 1; dz < z + 2; dz++) {
-                for (let dw = w - 1; dw < w + 2; dw++) {
-                    neighbors.push(`${dx},${dy},${dz},${dw}`);
-                }
-            }
-        }
-    }
-    return neighbors.filter((n) => n !== cube);
-}
-
 const puzzleInput3 = loadLayer(loadFile('day17/input.txt'), '0');
 const puzzleInput4 = loadLayer(loadFile('day17/input.txt'), '0,0');
 
 export const part1 = () => {
     let next = puzzleInput3;
-    for (let i = 0; i < 6; i++) next = runCycle(next, getCubeNeighbors3);
+    for (let i = 0; i < 6; i++) next = runCycle(next);
     return next.size;
 };
 
 export const part2 = () => {
     let next = puzzleInput4;
-    for (let i = 0; i < 6; i++) next = runCycle(next, getCubeNeighbors4);
+    for (let i = 0; i < 6; i++) next = runCycle(next);
     return next.size;
 };
